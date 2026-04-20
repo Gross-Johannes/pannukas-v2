8c55d694ae63977ec13113d72b486eef80e4c23f
Johannes Gross <johannesgross02@gmail.com>
2026-04-20 21:38:25 +0300

    chore(repo): initialize Spring Boot application with PostgreSQL configuration

---
+++ backend/src/main/java/net/teymm/pannukas/PannukasApplication.java
@@ -0,0 +1,12 @@
+package net.teymm.pannukas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PannukasApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(PannukasApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/net/teymm/pannukas/PannukasApplicationTests.java
@@ -0,0 +1,14 @@
+package net.teymm.pannukas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PannukasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
